3688f769ddb09d3ebdd04e096ad641ff02b337bf
Lucas Monteiro <lucashtm@id.uff.br>
2020-07-30 17:09:34 -0300

    Waves effect

--- js/initialize.js
+++ js/initialize.js
@@ -1,9 +1,11 @@
 let playerElements = document.getElementsByClassName('grid-container')[0].children;
 let players = [];
 const colors = ['#e20068', '#5300d8', '#4eca00'];
-
+var fillSVG = '<svg class="waves" style="width:100%;height:100%;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto"><defs><path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" /></defs><g class="parallax"><use xlink:href="#gentle-wave" x="48" y="2" fill="AAA" /><use xlink:href="#gentle-wave" x="48" y="4" fill="BBB" /></g></svg>'
 function setBarColor(bar, color){
-    bar.element.children[0].style.backgroundColor = color;
+    var bgColor = shadeColor(color, -30);
+    bar.element.getElementsByTagName('svg')[0].outerHTML = fillSVG.replace('AAA', shadeColor(color, 0)).replace('BBB', shadeColor(color, 30));
+    bar.element.children[0].style.backgroundColor = bgColor;
 }
 
 function setBarWidth(bar){
@@ -18,13 +20,16 @@ function createBar(player, options){
     const bars = player.getElementsByClassName('bars')[0];
     const newBar = document.createElement('div');
     newBar.classList.add('bar');
+    // const fill = document.createElement('div');
     const fill = document.createElement('div');
     fill.classList.add('bar-fill');
     newBar.appendChild(fill);
+    const svg = document.createElement('svg');
+    fill.appendChild(svg);
 
     // CALCULATE DEFAULT ATTRIBUTES / SKIP IF OPTIONS GIVEN
     const totalBars = bars.children.length;
-    const color = colors[totalBars % 3];
+    const color = colors[totalBars % 3] || options.color;
     const virtualPlayer = players.find(element => element.id == player.id);
     var virtualBar;
     if(!options){
@@ -54,23 +59,33 @@ function createBarForm(bar){
     const currentInput = document.createElement('input');
     currentInput.type = 'number';
     currentInput.value = bar.current;
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-bar');
+    deleteButton.innerHTML = '<i data-feather="trash-2"></i>';
     form.appendChild(colorInput);
     form.appendChild(maxInput);
     form.appendChild(currentInput);
+    form.appendChild(deleteButton);
     colorInput.addEventListener('change', () => {
         bar.color = colorInput.value;
         setBarColor(bar, colorInput.value);
-        saveState()
+        saveState();
     });
     maxInput.addEventListener('change', () => {
         bar.max = maxInput.value;
         setBarWidth(bar);
-        saveState()
+        saveState();
     });
     currentInput.addEventListener('change', () => {
         bar.current = currentInput.value;
         setBarWidth(bar);
-        saveState()
+        saveState();
+    });
+    deleteButton.addEventListener('click', e => {
+        e.preventDefault();
+        bar.element.remove();
+        console.log(bar);
+        saveState();
     });
     form.style.display = 'none';
     bar.element.addEventListener('click', () => {
@@ -86,6 +101,7 @@ function bindInputs(playerElement, player){
 
     button.addEventListener('click', () => {
         createBar(playerElement);
+        feather.replace();
         saveState();
     });
 
@@ -156,6 +172,7 @@ function restoreState(){
         bindInputs(player.element, player);
         restorePlayerBars(player)
     })
+    feather.replace();
 }
 
 function restorePlayerBars(player){
@@ -164,6 +181,27 @@ function restorePlayerBars(player){
         setBarWidth(bar);
         setBarColor(bar, bar.color);
     });
+}
+
+function shadeColor(color, percent) {
+
+    var R = parseInt(color.substring(1,3),16);
+    var G = parseInt(color.substring(3,5),16);
+    var B = parseInt(color.substring(5,7),16);
+
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R<255)?R:255;  
+    G = (G<255)?G:255;  
+    B = (B<255)?B:255;  
+
+    var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+    var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+    var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
 }
 
 restoreState();
